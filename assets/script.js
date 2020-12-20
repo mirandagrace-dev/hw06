e7636c14375f7902cf0a236f3a1d399a9d5f25e4
Miranda McDonald <mirandagrace.dev@gmail.com>
2020-12-20 16:45:31 -0500

    first commit: created needed files and added pseudo code

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// get query url
+var queryURL =
+  "api.openweathermap.org/data/2.5/weather?q=" +
+  cityName +
+  "&appid=e7a32108462aab31f968bcdc784f383c";
+
+//get city name from user
+//use dom manipulation to create input and to pass in the city name to the queryURL
+
+//use that input to call the queryUrl and show the results in a bootstrap card?
+//put bootstrap layouts in html and give them ids
+//get a blank card to show up onclick
+//get something to save to local storage
+//the saved cities get stored in an array
+// when a stored city is deleted, remove from the array
