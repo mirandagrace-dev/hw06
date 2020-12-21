23a68c0a93a69bb6fae67441b5c39bdec4b5b7e5
Miranda McDonald <mirandagrace.dev@gmail.com>
2020-12-21 00:11:06 -0500

    started structuring out the html as a blueprint for the dom manip

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 // get query url
 var queryURL =
-  "api.openweathermap.org/data/2.5/weather?q=" +
-  cityName +
-  "&appid=e7a32108462aab31f968bcdc784f383c";
+	"api.openweathermap.org/data/2.5/weather?q=" +
+	cityName +
+	"&appid=e7a32108462aab31f968bcdc784f383c";
 
 //get city name from user
 //use dom manipulation to create input and to pass in the city name to the queryURL
